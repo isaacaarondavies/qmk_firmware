65688dd219ab86a89a41fcc0d9b9d140e41a4a9b
isaacaarondavies <isaac151@byu.edu>
2024-04-28 14:44:47 -0600

    keyboard update

--- keyboards/ice_split_36/config.h
+++ keyboards/ice_split_36/config.h
@@ -4,18 +4,22 @@
 #pragma once
 
 #define MASTER_LEFT
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
 
-/* disable debug print */
-//#define NO_DEBUG
+/* Serial communication */
+#define SERIAL_USART_FULL_DUPLEX
 
-/* disable print */
-//#define NO_PRINT
+/* Uncomment when flashing left keyboard */
+#define SERIAL_USART_TX_PIN D3
+#define SERIAL_USART_RX_PIN D2
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+/* Uncomment when flashing right keyboard */
+// #define SERIAL_USART_TX_PIN D3
+// #define SERIAL_USART_RX_PIN D2
+
+/* Right keyboard kin definitions */
+#define MATRIX_COL_PINS_RIGHT \
+    { D4, D0, D1, B6, B2 }
+#define MATRIX_ROW_PINS_RIGHT \
+    { B5, B4, E6, D7 }
+
+// #define SPLIT_TRANSPORT_MIRROR
--- keyboards/ice_split_36/rules.mk
+++ keyboards/ice_split_36/rules.mk
@@ -1,1 +1,4 @@
 # This file intentionally left blank
+# For communication between the left and right keyboard
+SERIAL_DRIVER = vendor
+COMBO_ENABLE = yes
