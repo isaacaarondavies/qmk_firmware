8be7b7a52ef0a82dc1311a2febdbd0511fe2a8d1
isaacaarondavies <isaac151@byu.edu>
2024-04-28 17:11:46 -0600

    updates

--- keyboards/ice_split_36/keymaps/default/keymap.c
+++ keyboards/ice_split_36/keymaps/default/keymap.c
@@ -1,33 +1,70 @@
-// Copyright 2023 QMK
+// Copyright 2023 Idan Kamara (@idank)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
-enum layers {
-  TOP_QWERTY = 0,
-  NUMBER,
-  GAMING
-}
+enum {
+    _ALPHA,   // default
+    _SPECIAL, // special characters
+    _NUMBERS  // numbers/function/motion
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
-      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
-      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
-      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *           ┌───┐                   ┌───┐
-      *           │GUI├───┐           ┌───┤Alt│
-      *           └───┤Bsp├───┐   ┌───┤Ent├───┘
-      *               └───┤   │   │   ├───┘
-      *                   └───┘   └───┘
-      */
-    [0] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-    )
+    /* Keymap 0: Alpha layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |     Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |   P   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * | CTRL A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  |CTRL ; |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * | SHFT Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  |SHFT / |
+     * `-------------------------------'      `-------------------------------'
+     *   .------------------------------.    .----------------------.
+     *   | ESC META | ENT ALT | SPC SPE |    | SPC NUM | SHFT | TAB |
+     *   '------------------------------'    '----------------------'
+     */
+    [_ALPHA] = LAYOUT_split_3x5_3(
+         KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,     KC_O,   KC_P,
+         KC_A, KC_S,   KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,     KC_L,   KC_CTL,
+         KC_Z, KC_X,   KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMMA, KC_DOT, KC_SFT,
+             KC_ESC, KC_ENT, KC_SPC,          KC_SPC, KC_LSFT, KC_TAB),
+
+    /* Keymap 1: Special characters layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |    !  |  @  |  {  |  }  |  |  |      |  `  |  ~  |     |     |   \   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |    #  |  $  |  (  |  )  | RMB |      |  +  |  -  |  /  |  *  |   '   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |    %  |  ^  |  [  |  ]  | LMB |      |  &  |  =  |  ,  |  .  |   -   |
+     * `-------------------------------'      `-------------------------------'
+     *              .------------------.      .-----------------.
+     *              | BSPC |  ;  |  =  |      |  =  |  ;  | DEL |
+     *              '------------------'      '-----------------'
+     */
+    [_SPECIAL] = LAYOUT_split_3x5_3(
+         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,          KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
+         KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,          KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
+         KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,          KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
+                            KC_BSPC, KC_SCLN, KC_EQL,          KC_EQL, KC_SCLN, KC_DEL),
+
+    /* Keymap 2: Numbers/Function/Motion layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |   1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |  F1   | F2  | F3  | F4  | F5  |      | LFT | DWN | UP  | RGT | VOLUP |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |  F6   | F7  | F8  | F9  | F10 |      | MLFT| MDWN| MUP | MRGT| VOLDN |
+     * `-------------------------------'      `-------------------------------'
+     *               .-----------------.      .-----------------.
+     *               | F11 | F12 |     |      |     | PLY | SKP |
+     *               '-----------------'      '-----------------'
+     */
+    [_NUMBERS] = LAYOUT_split_3x5_3(
+         KC_1,  KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
+         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+                    KC_F11, KC_F12, KC_TRNS,         KC_TRNS,  KC_MPLY,  KC_MNXT)
 };
+
